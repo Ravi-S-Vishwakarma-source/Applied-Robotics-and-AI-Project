487f8f5fab5a1b903c0bc9c53fd79869d182b0cf
Ravi Shankar Vishwakarma <79796384+Ravi-S-Vishwakarma-source@users.noreply.github.com>
2023-12-31 12:54:23 +0000

    Add files via upload
    Red Resource files

---
+++ Red_Controller.d
@@ -0,0 +1,8 @@
+build/release/Red_Controller.o: Red_Controller.c \
+ C:\Users\vishw\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\vishw\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\vishw\AppData\Local\Programs\Webots/include/controller/c/webots/emitter.h \
+ C:\Users\vishw\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\vishw\AppData\Local\Programs\Webots/include/controller/c/webots/receiver.h \
+ C:\Users\vishw\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\vishw\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
